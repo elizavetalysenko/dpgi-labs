ebd60451d5f28cf326e455367278c7db0df9cbe9
Your Name <liza@gmail.com>
2024-05-16 23:46:16 +0300

    task1: DockPanel

--- lab2/obj/Debug/net7.0-windows/lab2.AssemblyInfo.cs
+++ lab2/obj/Debug/net7.0-windows/lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c054eb941b757be4f448ab45ec6e9ce90624db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde3bc254edb6a26f709c21a03a59965944befd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
